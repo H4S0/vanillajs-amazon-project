106c9af763b145e5d5021a9f500e3f7e355efb3f
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-06-03 16:07:40 +0200

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,94 +20,80 @@ cart.forEach((cartItem) => {
   let matchingProduct;
 
   products.forEach((product) => {
-    if(product.id === productId) {
+    if (product.id === productId) {
       matchingProduct = product;
     }
   });
+
   checkOutHTML += `
   <div class="cart-item-container cart-item-product-id-${matchingProduct.id}">
-            <div class="delivery-date">
-              Delivery date: Tuesday, June 21
-            </div>
+    <div class="delivery-date">
+      Delivery date: Tuesday, June 21
+    </div>
 
-            <div class="cart-item-details-grid">
-              <img class="product-image"
-                src="${matchingProduct.image}">
+    <div class="cart-item-details-grid">
+      <img class="product-image" src="${matchingProduct.image}">
 
-              <div class="cart-item-details">
-                <div class="product-name">
-                  ${matchingProduct.name}
-                </div>
-                <div class="product-price">
-                  ${formatCurrency(matchingProduct.priceCents)}
-                </div>
-                <div class="product-quantity">
-                  <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-                  </span>
-                  <span class="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span class="delete-quantity-link link-primary"
-                  data-product-id="${matchingProduct.id}">
-                    Delete
-                  </span>
-                </div>
-              </div>
-
-              <div class="delivery-options">
-                <div class="delivery-options-title">
-                  Choose a delivery option:
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div class="cart-item-details">
+        <div class="product-name">${matchingProduct.name}</div>
+        <div class="product-price">${formatCurrency(matchingProduct.priceCents)}</div>
+        <div class="product-quantity">
+          <span>Quantity: <span class="quantity-label">${cartItem.quantity}</span></span>
+          <div class="product-quantity-container">
+            <select class="checkout-quantity-selector" data-product-id="${matchingProduct.id}">
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            </select>
           </div>
-  `;
+          <span class="update-quantity-link link-primary" data-product-id="${matchingProduct.id}">
+            Update
+          </span>
+          <span class="delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
+            Delete
+          </span>
+        </div>
+      </div>
+
+      <div class="delivery-options">
+        <div class="delivery-options-title">Choose a delivery option:</div>
+        <div class="delivery-option">
+          <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">Tuesday, June 21</div>
+            <div class="delivery-option-price">FREE Shipping</div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">Wednesday, June 15</div>
+            <div class="delivery-option-price">$4.99 - Shipping</div>
+          </div>
+        </div>
+        <div class="delivery-option">
+          <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">Monday, June 13</div>
+            <div class="delivery-option-price">$9.99 - Shipping</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
 });
 
 let checkOutPage = document.querySelector('.order-summary');
 checkOutPage.innerHTML = checkOutHTML;
 
-let deleteButtons = document.querySelectorAll('.delete-quantity-link');
+const deleteButtons = document.querySelectorAll('.delete-quantity-link');
 
 deleteButtons.forEach((link) => {
   link.addEventListener('click', () => {
@@ -118,9 +104,29 @@ deleteButtons.forEach((link) => {
     cart.push(...newCart);
 
     const cartContainer = document.querySelector(`.cart-item-product-id-${productId}`);
-    cartContainer.remove();
+    if (cartContainer) {
+      cartContainer.remove();
+    }
     updateCheckOutItems();
     saveCartToLocalStorage();
+  });
+});
+
+const updateButtons = document.querySelectorAll('.update-quantity-link');
+const quantityLabel = document.querySelector('.quantity-label');
+updateButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    const updateQuantityEl = document.querySelector(`.checkout-quantity-selector[data-product-id="${productId}"]`);
+
+
+      const updateButtonQuantity = parseInt(updateQuantityEl.value);
+      const cartItem = cart.find(item => item.productId === productId);
+
+      cartItem.quantity = updateButtonQuantity;
+      quantityLabel.innerText = cartItem.quantity;
+      updateCheckOutItems();
+      saveCartToLocalStorage();
   });
 });
 
